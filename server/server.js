1a4ab78a6609cf8dc8dd7223d2b682f2e5961858
Yuxin Ye <yuxin_ye@protonmail.com>
2023-01-26 15:47:08 +1100

    add fe code into public folder

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
