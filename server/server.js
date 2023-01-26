815301e7cc001002f91bce18d284a5809f69fe60
Yuxin Ye <yuxin_ye@protonmail.com>
2023-01-26 15:29:09 +1100

    set up server.js port and requirements

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.request(express.json());
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+// Hardcode some items
+const items = new Map([
+  [1, { priceInCents: 10000, name: "Test One" }],
+  [2, { priceIncents: 20000, name: "Test Two" }],
+]);
+
+app.listen(3000);
